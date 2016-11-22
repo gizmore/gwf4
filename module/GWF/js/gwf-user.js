d5bd666a41769de650355a05e5cd635d8ab730fd
gizmore <gizmore@wechall.net>
2016-11-22 22:16:57 +0100

    gwf guest user name js

--- module/GWF/js/gwf-user.js
+++ module/GWF/js/gwf-user.js
@@ -2,6 +2,7 @@ var GWF_User = function(json) {
 	
 	this.JSON = json;
 	this.GUEST_NAME = json.user_name;
+	this.HAS_GUEST_NAME = false;
 	
 	this.authenticated = function() { return this.id() > 0; };
 	this.guest = function() { return !this.authenticated(); };
@@ -9,6 +10,10 @@ var GWF_User = function(json) {
 	this.id = function(id) { if(id) this.JSON.user_id = id; return this.JSON.user_id; };
 	this.secret = function() { return GWF_CONFIG.wss_secret; };
 	this.name = function(name) { if(name) this.JSON.user_name = name; return this.JSON.user_name; };
-	this.guestName = function(name) { if(name) this.GUEST_NAME = name; return this.GUEST_NAME; };
+	
+	this.guestName = function(name) { if(name) { this.GUEST_NAME = name; this.HAS_GUEST_NAME = true; } return this.GUEST_NAME; };
+	this.hasGuestName = function() { return this.HAS_GUEST_NAME; };
+
 	this.displayName = function() { return this.guest() ? this.guestName() : this.name(); };
+
 };
