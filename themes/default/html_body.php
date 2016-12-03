a75d280fedd8ee90825783879ea706fdd3c4bdc9
gizmore <gizmore@wechall.net>
2016-12-03 10:21:24 +0100

    Backdrop for sidenav.

--- themes/default/html_body.php
+++ themes/default/html_body.php
@@ -8,7 +8,7 @@
 
 		<!-- LEFT -->
 
-		<md-sidenav class="md-sidenav-left" md-component-id="left" md-disable-backdrop md-whiteframe="4">
+		<md-sidenav class="md-sidenav-left" md-component-id="left" md-whiteframe="4">
 			<div class="tgc-sidebar-menu-toggles">
 				<md-button ng-click="toggleLeftMenu()" class="toggle md-raised" aria-label="Close Left Menu"><i class="material-icons">account_circle</i></md-button>
 			</div>
@@ -19,7 +19,7 @@
 
 		<!-- RIGHT -->
 
-		<md-sidenav class="md-sidenav-right" md-component-id="right" md-disable-backdrop md-whiteframe="4">
+		<md-sidenav class="md-sidenav-right" md-component-id="right"  md-whiteframe="4">
 			<div class="tgc-sidebar-menu-toggles">
 				<md-button ng-click="toggleRightMenu()" class="md-raised" aria-label="Close Right Menu"><i class="material-icons">android</i></md-button>
 			</div>
