2c5b2d34a803779516bc3ac931546b1dc932d164
gizmore <gizmore@wechall.net>
2017-02-17 16:55:59 +0100

    Update all repos with one script

--- update_gwf4.sh
+++ update_gwf4.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 CORE="$(dirname "$0")"
 
 echo "git pull all repos"
-find . -iname ".git" -type d -exec sh -c "cd $CORE/{}/.. && echo `pwd`... && git pull" \;
+find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git pull" \;
